c9544b369b95ef3d218cbdbd44abd582564060bc
Halkano Malise <kudezx@gmail.com>
2023-04-29 00:14:46 +0300

    init

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
         <header>
             <div className="container">
-                <h2>Feedback Ui</h2>
+                <Link to="/">
+                    <h2>Feedback Ui</h2>
+                </Link>
             </div>
         </header>
     );
