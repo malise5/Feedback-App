666ea66592bc81e8cf647ab8a884a421d3a6c6e3
Halkano Malise <kudezx@gmail.com>
2023-04-28 17:00:46 +0300

    init

--- src/components/FeedbackItem.js
+++ src/components/FeedbackItem.js
@@ -1,7 +1,12 @@
 import React from "react";
 
-const FeedbackItem = () => {
-    return <div></div>;
-};
+function FeedbackItem({ feed }) {
+    return (
+        <div className="card">
+            <div className="num-display">{feed.rating}</div>
+            <div className="text-display">{feed.text}</div>
+        </div>
+    );
+}
 
 export default FeedbackItem;
