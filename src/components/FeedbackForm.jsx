a21067afd2baa035d3d708a0530659b7c623140a
Halkano Malise <kudezx@gmail.com>
2023-04-28 19:18:33 +0300

    added new button component

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import { useState } from "react";
 import Card from "./share/Card";
+import Button from "./share/Button";
 
 export default function FeedbackForm() {
     const [text, setText] = useState("");
+    const [btnDisabled, setBtnDisabled] = useState(true);
+    const [message, setMessage] = useState("");
 
     const handleChange = (e) => {
+        if (text === "") {
+            setBtnDisabled(true);
+            setMessage(null);
+        } else if (text !== "" && text.trim().length <= 10) {
+            setMessage("Text must be at least 10 characters long");
+            setBtnDisabled(true);
+        } else {
+            setMessage(null);
+            setBtnDisabled(false);
+        }
         setText(e.target.value);
     };
 
@@ -20,8 +33,11 @@ export default function FeedbackForm() {
                         value={text}
                         onChange={handleChange}
                     />
-                    <button type="submit">Send</button>
+                    <Button type="submit" isDisabled={btnDisabled}>
+                        Send
+                    </Button>
                 </div>
+                {message && <div className="message">{message}</div>}
             </form>
         </Card>
     );
